b85afa2257656f31f0c3d2f193e68619af0b62f2
Dainis Dilevka <thedenisovan@proton.me>
2025-08-13 14:55:33 +0100

    Moved weather svg to different path

--- src/modules/uiChanges.js
+++ src/modules/uiChanges.js
@@ -118,15 +118,15 @@ function futureWindSpeed(data) {
 
 function returnCorrectWeatherIcon(icon) {
   const icons = {
-    'clear-day': require('../assets/weather-icons/design/fill/final/clear-day.svg'),
-    'clear-night': require('../assets/weather-icons/design/fill/final/clear-night.svg'),
-    'partly-cloudy-night': require('../assets/weather-icons/design/fill/final/partly-cloudy-night.svg'),
-    'partly-cloudy-day': require('../assets/weather-icons/design/fill/final/partly-cloudy-day.svg'),
-    'cloudy': require('../assets/weather-icons/design/fill/final/cloudy.svg'),
-    'wind': require('../assets/weather-icons/design/fill/final/wind.svg'),
-    'fog': require('../assets/weather-icons/design/fill/final/fog.svg'),
-    'rain': require('../assets/weather-icons/design/fill/final/rain.svg'),
-    'snow': require('../assets/weather-icons/design/fill/final/snow.svg'),
+    'clear-day': require('../assets/weather-icons/clear-day.svg'),
+    'clear-night': require('../assets/weather-icons/clear-night.svg'),
+    'partly-cloudy-night': require('../assets/weather-icons/partly-cloudy-night.svg'),
+    'partly-cloudy-day': require('../assets/weather-icons/partly-cloudy-day.svg'),
+    'cloudy': require('../assets/weather-icons/cloudy.svg'),
+    'wind': require('../assets/weather-icons/wind.svg'),
+    'fog': require('../assets/weather-icons/fog.svg'),
+    'rain': require('../assets/weather-icons/rain.svg'),
+    'snow': require('../assets/weather-icons/snow.svg'),
   }
 
   return icons[icon] || icons['partly-cloudy-day'];
